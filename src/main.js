dee91a7b25725c80cd32aeefc3cb61529ab3d53a
ashopov <alex@frontstage.digital>
2025-07-27 10:59:26 +1000

    fullpage.js

--- src/main.js
+++ src/main.js
@@ -1,24 +1,14 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import trevi from '/cristina-gottardi-I1Lv2yX67GI-unsplash.jpg'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <div id="fullpage">
+    <div class="section">
+      <img src="${trevi}"class="section-bg" alt="">
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <div class="section">Some section</div>
+    <div class="section">Some section</div>
+    <div class="section">Some section</div>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
